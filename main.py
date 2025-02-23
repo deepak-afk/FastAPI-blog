52774154deda35513a4402b3e02bcc22f4c1be85
deepak-afk <deepakvenki71@gmail.com>
2025-02-23 16:33:52 +0530

    query params

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return "heyy"
+@app.get('/blog')
+def index(limit = 10, published : bool = True, sort : Optional[str] = None):
+    if published:
+        return {'data' : f'{limit} published blogs from db'}
+    else:
+        return {'data' : f'{limit} blogs from db'}
+
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data' : 'unpublished blogs'}
+
+
+
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data' : id }
+
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit = 10):
+    return {'data' : {'1', '2'}}                                                                                                                                                                                                                         
