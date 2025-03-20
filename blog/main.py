9108a3bf194f9087525ce3a2f3a2b9591c9c8910
deepak-afk <deepakvenki71@gmail.com>
2025-03-20 22:34:37 +0530

    update and delete

--- blog/main.py
+++ blog/main.py
@@ -23,6 +23,26 @@ def create(request : schemas.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).fliter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail = f"Blog with the {id} is not available")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'blog is deleted'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id,request : schemas.Blog,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).fliter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail = f"Blog with the {id} is not available")
+    blog.update(request)
+    db.commit()
+    return "updated successfully"
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
